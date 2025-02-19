e2c6125756aace0b781b030a1e6b54daac0c3e2a
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-02-19 21:16:30 +0000

    added ManyStrings with string variables

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello LP1!");
+
+            string str_1 = "olá mundo! \u272A";
+            string str_2 = "a space\t and pretty letters \u24BD \u24BE ";
+            string str_3 = "a different alphabet \u03A6 \u03A8 \u03BE ";
+
+            Console.WriteLine(str_1);
+            Console.WriteLine(str_2);
+            Console.WriteLine(str_3);
+        }
+    }
+}
