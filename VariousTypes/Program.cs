c221ab301887b6bb7d27a39bf2445bbe32a457f5
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-02-19 20:58:20 +0000

    added real variables to VariousTypes

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -18,12 +18,19 @@ namespace VariousTypes
             char arrow ='\u21AB';
             char hand ='\u261A';
 
+            double x = 3.1415;
+            float k =3.1416f;
+            decimal a = 3.1417m;
+
             Console.WriteLine(num);
             Console.WriteLine(z);
             Console.WriteLine(y);
             Console.WriteLine(j);
             Console.WriteLine(arrow);
             Console.WriteLine(hand);
+            Console.WriteLine(x);
+            Console.WriteLine(k);
+            Console.WriteLine(a);
 
 
 
