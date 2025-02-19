fc21815b486d1bc2b0af350bc8ea0baac1ec40c8
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-02-19 20:49:25 +0000

    added char variables to VariousTypes

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace VariousTypes
 {
@@ -6,6 +7,7 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Hello LP1!");
 
             int num = 8;
@@ -13,10 +15,15 @@ namespace VariousTypes
             long y = 1L;
             ulong j = 5UL;
 
+            char arrow ='\u21AB';
+            char hand ='\u261A';
+
             Console.WriteLine(num);
             Console.WriteLine(z);
             Console.WriteLine(y);
             Console.WriteLine(j);
+            Console.WriteLine(arrow);
+            Console.WriteLine(hand);
 
 
 
