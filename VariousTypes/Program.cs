658a3261af22d5b2aebd7382a218d13ddf5e45f0
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-02-19 20:37:45 +0000

    added VariousTypes with variaveis inteiras

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+
+            int num = 8;
+            uint z = 7U;
+            long y = 1L;
+            ulong j = 5UL;
+
+            Console.WriteLine(num);
+            Console.WriteLine(z);
+            Console.WriteLine(y);
+            Console.WriteLine(j);
+
+
+
+        }
+    }
+}
