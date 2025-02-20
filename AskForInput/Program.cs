c42546e5d53e2de857e76cd8a40396f334277280
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-02-20 20:18:21 +0000

    added AskForInput with first input request

---
+++ AskForInput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+
+            Console.WriteLine("Insira um número inteiro");
+            string input_1 = Console.ReadLine();
+
+            int convertido_1 = int.Parse(input_1);
+
+            
+
+        }
+    }
+}
