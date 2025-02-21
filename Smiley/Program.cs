6be1b77c978678a4505cf2036e8a9165df2ba30b
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-02-21 17:54:45 +0000

    added smiley file with the added coding

---
+++ Smiley/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello LP1!");
+
+            Console.WriteLine("insere código :");
+            string input_1 = Console.ReadLine();
+
+           int converted1 = System.Int32.Parse(input_1, NumberStyles.
+           AllowHexSpecifier);
+
+           int charvalue = Convert.ToChar(converted1);
+           
+
+           Console.WriteLine("carácter Unicode : " + charvalue);
+
+            
+
+            
+        }
+    }
+}
